41f87917087fbcce7587cc4f19abbcb880abb7eb
Artem <xelfaer18@gmail.com>
2022-11-22 19:50:01 +1100

    Два готовы

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Console.WriteLine("Введите число  ");
+int numN = Convert.ToInt32(Console.ReadLine());
+int i = 1, k = 1; 
+while (true)
+{ 
+    if (i % 2 == 0)
+    { Console.Write(i + " ");
+    k++;
+    }
+    if (i == numN)
+    { break; }
+     i++;}*/
+
+/*Console.WriteLine("Введите число a");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{ 
+    Console.WriteLine($"Max = {num1} Min = {num2}");
+    }
+else
+{ 
+    Console.WriteLine($"Max = {num2} Min = {num1}");
+    }*/
